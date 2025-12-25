8b4068d7ddc036efe4720435eb57036ebfe50d07
TarekElsayed <147595410+TaRek-ElSayed02@users.noreply.github.com>
2025-12-25 14:19:49 +0200

    fix footer

--- app/components/Footer/Footer.tsx
+++ app/components/Footer/Footer.tsx
@@ -15,7 +15,7 @@ export default function Footer() {
     const loc = "/loc.png"
 
     return (
-        <div className="relative w-full h-[669px] md:h-[323px] overflow-hidden">
+        <div className="relative w-full h-[auti] md:h-[323px] overflow-hidden ">
             <div
                 className="absolute inset-0 bg-cover bg-center bg-no-repeat"
                 style={{ backgroundImage: `url(${kid})` }}
